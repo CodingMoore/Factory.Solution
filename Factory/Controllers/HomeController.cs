dd4b8abe58d5417b18a6c977a98b0cf2eb7a919f
Randel M <Randel.C.Moore@gmail.com>
2021-01-10 17:41:51 -0800

    add list of machines and engineers to home page

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,13 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Factory.Models;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Factory.Controllers
 {
   public class HomeController : Controller
   {
-    
+    private readonly FactoryContext _db; // Defining the Database as Factory
+    public HomeController(FactoryContext db) //constructor for the controller 
+    {
+      _db = db;
+    }
+
     [HttpGet("/")]
     public ActionResult Index()
     {
+      // List<Machine> machineList = _db.Machines;
+      // List<Engineer> engineerList = _db.Engineers;
+      ViewBag.Machines = _db.Machines;
+      ViewBag.Engineers = _db.Engineers;;
       return View();
     }
 
